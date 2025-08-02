4b5d170ebecebbe352f09e2a8dd350a2f58c0095
Abraham Medina <abrahammdnlpz@gmail.com>
2025-08-02 05:51:49 -0600

    Entregable 5 Campus: Modulo 5, Proyecto ChatGPT: Endpoint POST en Express

--- ChatGPT-Clon/express/server.js
+++ ChatGPT-Clon/express/server.js
@@ -3,11 +3,25 @@ const express = require('express');
 // Se inicializa express
 const app = express();
 // Se selecciona un puerto para el servidor
-const port = 8080;
+const port = 3000;
 
-// Se crea un endpoint para traer un saludo que compruebe que el servidor funciona
+// Se crea un middleware para parsear JSONs
+app.use(express.json());
+
+/* Ruta GET */
+// Se crea un endpoint GET sencillo para traer un saludo que compruebe que el servidor funciona
 app.get('/', (req, res) => {
     res.send('¡Hola Mundo!')
+});
+
+/* Ruta POST */
+// Se crea un endpoint POST sencillo para enviar información 
+app.post('/data', (req, res) => {
+  console.log('Información recibida:', req.body);
+  res.json({
+    message: 'Datos recibidos correctamente',
+    data: req.body,
+  });
 });
 
 // Se inicia el servidor
